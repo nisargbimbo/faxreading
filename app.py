b45501c9ecd2ffcf323fb23b00cda6055d1a7d22
nisargbimbo <nisarg.patel@grupobimbo.com>
2023-08-07 08:58:45 -0400

    update

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def convert_pdf_image_path(pdf_path, save_folder, filename, number_of_pages):
 def convert_pdf_image_bytes(pdf_file, save_folder, filename, number_of_pages):
     
     # Store Pdf with convert_from_path function
-    images = convert_from_bytes(pdf_file.read(), poppler_path = './poppler-23.07.0/Library/bin')
+    images = convert_from_bytes(pdf_file.read(),) #poppler_path = './poppler-23.07.0/Library/bin')
     
     if os.path.isdir(os.path.join(save_folder, filename[:-4])):
         pass
